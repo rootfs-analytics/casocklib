2d0610a61afff20218ae9fd4677b06b8a2b499f9
leandro.costa <leandro.costa@fc5ed29a-2a59-11df-b744-258d51847b74>
2010-04-08 02:22:25 +0000

    Reorganizing files, still working in rpc using asio

---
+++ trunk/src/casock/rpc/sigio/protobuf/client/RPCClientProxy.cc
@@ -0,0 +1,102 @@
+/*
+ * casocklib - An asynchronous communication library for C++
+ * ---------------------------------------------------------
+ * Copyright (C) 2010 Leandro Costa
+ *
+ * This file is part of casocklib.
+ *
+ * casocklib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * casocklib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with casocklib. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*!
+ * \file casock/rpc/sigio/protobuf/client/RPCClientProxy.cc
+ * \brief [brief description]
+ * \author Leandro Costa
+ * \date 2010
+ *
+ * $LastChangedDate$
+ * $LastChangedBy$
+ * $Revision$
+ */
+
+#include "RPCClientProxy.h"
+
+#include <sstream>
+using std::stringstream;
+
+#include <google/protobuf/message.h>
+#include <google/protobuf/descriptor.h>
+
+#include "casock/util/Logger.h"
+#include "casock/sigio/client/ClientSocket.h"
+#include "casock/rpc/protobuf/api/rpc.pb.h"
+#include "casock/rpc/protobuf/client/RPCCall.h"
+#include "casock/rpc/protobuf/client/RPCCallQueue.h"
+#include "casock/rpc/protobuf/client/RPCCallHandler.h"
+//#include "casock/rpc/sigio/protobuf/client/RPCCallController.h"
+#include "casock/rpc/sigio/protobuf/client/RPCReaderHandler.h"
+
+namespace casock {
+  namespace rpc {
+    namespace sigio {
+      namespace protobuf {
+        namespace client {
+          RPCClientProxy::RPCClientProxy (casock::sigio::base::Dispatcher& rDispatcher, const string& host, const unsigned int& port)
+          {
+            LOGMSG (HIGH_LEVEL, "RPCClientProxy::RPCClientProxy ()\n");
+
+            mpCallQueue = new RPCCallQueue ();
+            mpCallHandler = new RPCCallHandler (*mpCallQueue);
+            mpCallHandler->start ();
+
+            mpClientSocket = new ClientSocket (rDispatcher, host, port);
+            mpClientSocket->connect ();
+            mpReaderHandler = new RPCReaderHandler (rDispatcher, mpClientSocket, this, mCallHash, *mpCallQueue);
+            mpChannel = new RPCChannel (&(mpReaderHandler->communicator ()));
+            mpService = new casock::rpc::protobuf::api::RpcService::Stub (mpChannel);
+          }
+
+          /*
+             void RPCClientProxy::callback ()
+             {
+             LOGMSG (HIGH_LEVEL, "RPCClientProxy::%s ()\n", __FUNCTION__);
+             }
+           */
+
+          void RPCClientProxy::CallMethod(const google::protobuf::MethodDescriptor* method, google::protobuf::RpcController* controller, const google::protobuf::Message* request, google::protobuf::Message* response, google::protobuf::Closure* done)
+          {
+            LOGMSG (HIGH_LEVEL, "RPCClientProxy::%s ()\n", __FUNCTION__);
+
+            if (mpClientSocket->connected ())
+            {
+              casock::rpc::protobuf::api::RpcRequest* pRpcRequest = new casock::rpc::protobuf::api::RpcRequest ();
+
+              pRpcRequest->set_id (1);
+              pRpcRequest->set_operation (method->name ());
+              pRpcRequest->set_request (request->SerializeAsString ());
+
+              mCallHash.lock ();
+              mCallHash [pRpcRequest->id ()] = new RPCCall (response, controller, done);
+              mCallHash.unlock ();
+
+              mpService->RpcCall (NULL, pRpcRequest, NULL, NULL);
+            }
+            else
+              LOGMSG (HIGH_LEVEL, "RPCClientProxy::%s () - socket disconnected!\n", __FUNCTION__);
+          }
+        }
+      }
+    }
+  }
+}
---
+++ trunk/src/casock/rpc/sigio/protobuf/server/RPCServerProxy.h
@@ -0,0 +1,103 @@
+/*
+ * casocklib - An asynchronous communication library for C++
+ * ---------------------------------------------------------
+ * Copyright (C) 2010 Leandro Costa
+ *
+ * This file is part of casocklib.
+ *
+ * casocklib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * casocklib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with casocklib. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*!
+ * \file casock/rpc/sigio/protobuf/server/RPCServerProxy.h
+ * \brief [brief description]
+ * \author Leandro Costa
+ * \date 2010
+ *
+ * $LastChangedDate$
+ * $LastChangedBy$
+ * $Revision$
+ */
+
+#ifndef __CASOCKLIB__CASOCK_RPC_SIGIO_PROTOBUF_SERVER__RPC_SERVER_PROXY_H_
+#define __CASOCKLIB__CASOCK_RPC_SIGIO_PROTOBUF_SERVER__RPC_SERVER_PROXY_H_
+
+namespace google {
+  namespace protobuf {
+    class Service;
+  }
+}
+
+namespace casock {
+  namespace sigio {
+    namespace base {
+      class Dispatcher;
+    }
+
+    namespace server {
+      class SocketServer;
+    }
+  }
+
+  namespace rpc {
+    namespace protobuf {
+      namespace server {
+        class RPCCallResponseHandler;
+
+        template<typename _TpResponseHandler>
+          class RPCCallQueue;
+
+        template<typename _TpResponseHandler>
+          class RPCCallHandler;
+      }
+    }
+
+    namespace sigio {
+      namespace protobuf {
+        namespace server {
+          class RPCAcceptorHandler;
+
+          using casock::rpc::protobuf::server::RPCCallResponseHandler;
+          using casock::rpc::protobuf::server::RPCCallQueue;
+          using casock::rpc::protobuf::server::RPCCallHandler;
+
+          class RPCServerProxy
+          {
+            public:
+              RPCServerProxy (casock::sigio::base::Dispatcher& rDispatcher, const unsigned int& port, google::protobuf::Service* pService);
+              virtual ~RPCServerProxy ();
+
+            public:
+              void start ();
+              void stop ();
+
+            private:
+              casock::sigio::base::Dispatcher&        mrDispatcher;
+
+              casock::sigio::server::SocketServer*    mpSocketServer;
+              RPCAcceptorHandler*                     mpAcceptorHandler;
+              RPCCallQueue<RPCCallResponseHandler>*   mpCallQueue;
+              RPCCallHandler<RPCCallResponseHandler>* mpCallHandler;
+
+              google::protobuf::Service*              mpService;
+
+              bool m_running;
+          };
+        }
+      }
+    }
+  }
+}
+
+#endif // __CASOCKLIB__CASOCK_RPC_SIGIO_PROTOBUF_SERVER__RPC_SERVER_PROXY_H_
